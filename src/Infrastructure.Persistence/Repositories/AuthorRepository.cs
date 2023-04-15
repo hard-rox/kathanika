ffbccfdf74ed2949a68f3b3c58f34853a4cc9dc6
Rasedur Rahman Roxy <roxy.xprez@gmail.com>
2023-04-15 13:02:17 +0600

    repository class made sealed

--- src/Infrastructure.Persistence/Repositories/AuthorRepository.cs
+++ src/Infrastructure.Persistence/Repositories/AuthorRepository.cs
@@ -1,6 +1,6 @@
 namespace Kathanika.Infrastructure.Persistence.Repositories;
 
-internal class AuthorRepository : Repository<Author>, IAuthorRepository
+internal sealed class AuthorRepository : Repository<Author>, IAuthorRepository
 {
     private const string collectionName = "authors";
     public AuthorRepository(IMongoDatabase database, ILogger<AuthorRepository> logger) : base(database, collectionName, logger)
