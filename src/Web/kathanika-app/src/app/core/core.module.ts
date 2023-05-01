b7bf3202b8a3a895330a0581f152961a58312e4c
Rasedur Rahman Roxy <roxy.xprez@gmail.com>
2023-05-01 21:17:31 +0600

    Basic UI components

--- src/Web/kathanika-app/src/app/core/core.module.ts
+++ src/Web/kathanika-app/src/app/core/core.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooterComponent, HeaderComponent, SidebarComponent } from '.';
+import { SharedModule } from '../shared';
 
 
 
@@ -11,7 +12,8 @@ import { FooterComponent, HeaderComponent, SidebarComponent } from '.';
     SidebarComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule
   ],
   exports: [
     FooterComponent,
