c2626e8907c9876449665d82abca2de1b610b879
Rasedur Rahman Roxy <roxy.xprez@gmail.com>
2023-05-10 08:48:07 +0600

    Test issues resolved

--- src/Web/kathanika-app/src/app/features/authors/pages/author-details/author-details.component.spec.ts
+++ src/Web/kathanika-app/src/app/features/authors/pages/author-details/author-details.component.spec.ts
@@ -1,6 +1,15 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AuthorDetailsComponent } from './author-details.component';
+import { of } from 'rxjs';
+import { GetAuthorGQL } from 'src/app/graphql/generated';
+import { ActivatedRoute } from '@angular/router';
+
+class MockGetAuthorGQL {
+  watch = jasmine.createSpy('watch').and.returnValue({
+    valueChanges: of({}),
+  });
+}
 
 describe('AuthorDetailsComponent', () => {
   let component: AuthorDetailsComponent;
@@ -8,9 +17,24 @@ describe('AuthorDetailsComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ AuthorDetailsComponent ]
-    })
-    .compileComponents();
+      declarations: [AuthorDetailsComponent],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            snapshot: {
+              params: {
+                id: '',
+              },
+            },
+          },
+        },
+        {
+          provide: GetAuthorGQL,
+          useClass: MockGetAuthorGQL,
+        },
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(AuthorDetailsComponent);
     component = fixture.componentInstance;
--- src/Web/kathanika-app/src/app/features/authors/pages/author-list/author-list.component.spec.ts
+++ src/Web/kathanika-app/src/app/features/authors/pages/author-list/author-list.component.spec.ts
@@ -1,24 +1,45 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { ApolloTestingModule } from 'apollo-angular/testing';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
 
 import { AuthorListComponent } from './author-list.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { GetAuthorsGQL } from 'src/app/graphql/generated';
+
+class MockGetAuthorsGQL {
+  watch = jasmine.createSpy('watch').and.returnValue({
+    valueChanges: of({
+      data: {
+        authors: {
+          items: [],
+        },
+      },
+    }),
+  });
+}
 
 describe('AuthorListComponent', () => {
   let component: AuthorListComponent;
   let fixture: ComponentFixture<AuthorListComponent>;
+  let gql: MockGetAuthorsGQL;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ 
-        AuthorListComponent
+      declarations: [AuthorListComponent],
+      imports: [SharedModule],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            queryParams: of({}),
+          },
+        },
+        {
+          provide: GetAuthorsGQL,
+          useClass: MockGetAuthorsGQL,
+        },
       ],
-      imports: [
-        ApolloTestingModule,
-        SharedModule
-      ]
-    })
-    .compileComponents();
+    }).compileComponents();
 
     fixture = TestBed.createComponent(AuthorListComponent);
     component = fixture.componentInstance;
@@ -27,5 +48,9 @@ describe('AuthorListComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  xit('watch should be called once', () => {
+    expect(gql.watch).toHaveBeenCalled();
   });
 });
