83f10a691e4043ad865dcca686657f4d29eae2c0
Rasedur Rahman Roxy <roxy.xprez@gmail.com>
2023-05-12 12:55:55 +0600

    AuthorList component tests

--- src/Web/kathanika-app/src/app/features/authors/pages/author-list/author-list.component.spec.ts
+++ src/Web/kathanika-app/src/app/features/authors/pages/author-list/author-list.component.spec.ts
@@ -6,22 +6,9 @@ import { AuthorListComponent } from './author-list.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { GetAuthorsGQL } from 'src/app/graphql/generated/graphql-operations';
 
-class MockGetAuthorsGQL {
-  watch = jasmine.createSpy('watch').and.returnValue({
-    valueChanges: of({
-      data: {
-        authors: {
-          items: [],
-        },
-      },
-    }),
-  });
-}
-
 describe('AuthorListComponent', () => {
   let component: AuthorListComponent;
   let fixture: ComponentFixture<AuthorListComponent>;
-  let gql: MockGetAuthorsGQL;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -36,21 +23,52 @@ describe('AuthorListComponent', () => {
         },
         {
           provide: GetAuthorsGQL,
-          useClass: MockGetAuthorsGQL,
+          useValue: {
+            watch: () => {
+              return {
+                valueChanges: {},
+                refetch: () => { }
+              };
+            },
+          },
         },
       ],
-    }).compileComponents();
-
-    fixture = TestBed.createComponent(AuthorListComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    })
+      .compileComponents()
+      .then(() => {
+        fixture = TestBed.createComponent(AuthorListComponent);
+        component = fixture.componentInstance;
+      });
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 
-  xit('watch should be called once', () => {
-    expect(gql.watch).toHaveBeenCalled();
+  it('changePage should change _queryVariables and call _queryRef.refetch() and setQueryParams()', () => {
+    const setQueryParamsSpy = spyOn<any>(component, 'setQueryParams');
+    let queryVariables = component['_queryVariables'];
+    const queryRefRefetchSpy = spyOn<any>(component['_queryRef'], 'refetch');
+    const page = 2;
+
+    component.changePage(page);
+
+    expect(queryVariables.skip).toEqual((page - 1) * queryVariables.take);
+    expect(queryRefRefetchSpy).toHaveBeenCalledOnceWith(queryVariables);
+    expect(setQueryParamsSpy).toHaveBeenCalled();
+  });
+
+  it('changePageSize should change _queryVariables and call _queryRef.refetch() and setQueryParams()', () => {
+    const setQueryParamsSpy = spyOn<any>(component, 'setQueryParams');
+    let queryVariables = component['_queryVariables'];
+    const queryRefRefetchSpy = spyOn<any>(component['_queryRef'], 'refetch');
+    const pageSize = 20;
+
+    component.changePageSize(pageSize);
+
+    expect(queryVariables.skip).toEqual(0);
+    expect(queryVariables.take).toEqual(pageSize);
+    expect(queryRefRefetchSpy).toHaveBeenCalledOnceWith(queryVariables);
+    expect(setQueryParamsSpy).toHaveBeenCalled();
   });
 });
