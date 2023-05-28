14d0c9cb0fb8ffc39059f6feb65db813e735e5db
Rasedur Rahman Roxy <roxy.xprez@gmail.com>
2023-05-28 23:43:28 +0600

    MarkAuthorAsDeceasedCommandHandlerTests done

--- tests/UnitTests/ApplicationUnitTests/Commands/Author/MarkAuthorAsDeceasedCommandHandlerTests.cs
+++ tests/UnitTests/ApplicationUnitTests/Commands/Author/MarkAuthorAsDeceasedCommandHandlerTests.cs
@@ -1,5 +1,6 @@
 ﻿using Kathanika.Application.Commands;
 using Kathanika.Domain.Aggregates;
+using Kathanika.Domain.Exceptions;
 using Moq;
 
 namespace Kathanika.UnitTests.ApplicationUnitTests.Commands;
@@ -17,7 +18,7 @@ public class MarkAuthorAsDeceasedCommandHandlerTests
             "",
             "");
         var authorRepositoryMock = new Mock<IAuthorRepository>();
-        authorRepositoryMock.Setup(x => x.GetByIdAsync(It.IsAny<string>())).ReturnsAsync(author);
+        authorRepositoryMock.Setup(x => x.GetByIdAsync(It.IsAny<string>())).ReturnsAsync(author).Verifiable();
         authorRepositoryMock.Setup(x => x.UpdateAsync(It.IsAny<Author>())).Verifiable();
         var command = new MarkAuthorAsDeceasedCommand("", dateOfDeath);
         var handler = new MarkAuthorAsDeceasedCommandHandler(authorRepositoryMock.Object);
@@ -27,5 +28,21 @@ public class MarkAuthorAsDeceasedCommandHandlerTests
         Assert.NotNull(updatedAuthor);
         Assert.NotNull(updatedAuthor.DateOfDeath);
         Assert.Equal(author.DateOfDeath, dateOfDeath);
+        authorRepositoryMock.Verify(x => x.GetByIdAsync(It.IsAny<string>()), Times.Exactly(1));
+        authorRepositoryMock.Verify(x => x.UpdateAsync(It.Is<Author>(x => x == author)), Times.Exactly(1));
+    }
+
+    [Fact]
+    public async Task Handler_Should_Throw_Exception_On_Invalid_AuthorId()
+    {
+        var authorId = Guid.NewGuid().ToString();
+        var authorRepositoryMock = new Mock<IAuthorRepository>();
+        var command = new MarkAuthorAsDeceasedCommand(authorId, DateTime.MaxValue);
+        var handler = new MarkAuthorAsDeceasedCommandHandler(authorRepositoryMock.Object);
+
+        var exception = await Assert.ThrowsAsync<NotFoundWithTheIdException>(async () => await handler.Handle(command, default));
+
+        Assert.IsAssignableFrom<NotFoundWithTheIdException>(exception);
+        Assert.Equal(authorId, exception.Id);
     }
 }
