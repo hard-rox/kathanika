3c3aa9c5c5567e0812822b5425cf1d7fd30d37fb
Rasedur Rahman Roxy <roxy.xprez@gmail.com>
2023-09-07 08:21:05 +0600

    feat: Building blocks created for domain events.

--- src/Domain/Primitives/AggregateRoot.cs
+++ src/Domain/Primitives/AggregateRoot.cs
@@ -2,4 +2,10 @@ namespace Kathanika.Domain.Primitives;
 
 public abstract class AggregateRoot : Entity
 {
+    private readonly List<IDomainEvent> domainEvents = new();
+
+    protected void AddDomainEvent(IDomainEvent domainEvent)
+    {
+        domainEvents.Add(domainEvent);
+    }
 }
