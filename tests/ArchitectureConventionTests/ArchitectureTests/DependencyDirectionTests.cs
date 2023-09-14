8ca204309dfc3a8aac74a11b20a380ed0bd3ab98
Rasedur Rahman Roxy <roxy.xprez@gmail.com>
2023-09-14 07:56:26 +0600

    test: Domain and Application dependency direction tests

--- tests/ArchitectureConventionTests/ArchitectureTests/DependencyDirectionTests.cs
+++ tests/ArchitectureConventionTests/ArchitectureTests/DependencyDirectionTests.cs
@@ -1,5 +1,9 @@
 using Kathanika.Application.Authors.Commands;
 using Kathanika.Domain.Primitives;
+using Kathanika.Infrastructure.GraphQL;
+using Kathanika.Infrastructure.GraphQL.Types;
+using Kathanika.Infrastructure.Persistence;
+using Kathanika.Infrastructure.Workers.Jobs;
 
 namespace Kathanika.ArchitectureConventionTests.ArchitectureTests;
 
@@ -13,10 +17,74 @@ public sealed class DependencyDirectionTests
         Assembly applicationAssembly = typeof(AddAuthorCommand).Assembly;
 
         TestResult result = Types.InAssembly(_domainAssembly)
-        .ShouldNot()
-        .HaveDependencyOn(applicationAssembly.GetName().Name)
-        .GetResult();
+            .ShouldNot()
+            .HaveDependencyOn(applicationAssembly.GetName().Name)
+            .GetResult();
 
         Assert.True(result.IsSuccessful);
+    }
+
+    [Fact]
+    public void Domain_ShouldNot_HaveDependencyOnWeb()
+    {
+        Assembly webAssembly = typeof(Program).Assembly;
+
+        TestResult result = Types.InAssembly(_domainAssembly)
+            .ShouldNot()
+            .HaveDependencyOn(webAssembly.GetName().Name)
+            .GetResult();
+
+        Assert.True(result.IsSuccessful);
+    }
+
+    [Fact]
+    public void Domain_ShouldNot_HaveDependencyOnInfrastructure()
+    {
+        Assembly graphqlInfraAssembly = typeof(SchemaConfigurations).Assembly;
+        Assembly persistenceInfraAssembly = typeof(MongoDbConfigurations).Assembly;
+        Assembly workersInfraAssembly = typeof(ProcessOutboxMessagesJob).Assembly;
+
+        TestResult graphqlInfraDepsResult = Types.InAssembly(_domainAssembly)
+            .ShouldNot()
+            .HaveDependencyOn(graphqlInfraAssembly.GetName().Name)
+            .GetResult();
+        TestResult persistenceInfraDepsResult = Types.InAssembly(_domainAssembly)
+            .ShouldNot()
+            .HaveDependencyOn(persistenceInfraAssembly.GetName().Name)
+            .GetResult();
+        TestResult workerInfraDepsResult = Types.InAssembly(_domainAssembly)
+            .ShouldNot()
+            .HaveDependencyOn(workersInfraAssembly.GetName().Name)
+            .GetResult();
+
+        Assert.True(graphqlInfraDepsResult.IsSuccessful);
+        Assert.True(persistenceInfraDepsResult.IsSuccessful);
+        Assert.True(workerInfraDepsResult.IsSuccessful);
+    }
+
+    [Fact]
+    public void Application_ShouldNot_HaveDependencyOnInfrastructure()
+    {
+        Assembly applicationAssembly = typeof(AddAuthorCommand).Assembly;
+        Assembly graphqlInfraAssembly = typeof(SchemaConfigurations).Assembly;
+        Assembly persistenceInfraAssembly = typeof(MongoDbConfigurations).Assembly;
+        Assembly workersInfraAssembly = typeof(ProcessOutboxMessagesJob).Assembly;
+
+        TestResult graphqlInfraDepsResult = Types.InAssembly(applicationAssembly)
+            .ShouldNot()
+            .HaveDependencyOn(graphqlInfraAssembly.GetName().Name)
+            .GetResult();
+        TestResult persistenceInfraDepsResult = Types.InAssembly(applicationAssembly)
+            .ShouldNot()
+            .HaveDependencyOn(persistenceInfraAssembly.GetName().Name)
+            .GetResult();
+        TestResult workerInfraDepsResult = Types.InAssembly(applicationAssembly)
+            .ShouldNot()
+            .HaveDependencyOn(workersInfraAssembly.GetName().Name)
+            .GetResult();
+
+        Assert.True(graphqlInfraDepsResult.IsSuccessful);
+        Assert.True(persistenceInfraDepsResult.IsSuccessful);
+        Assert.True(workerInfraDepsResult.IsSuccessful);
     }
 }
