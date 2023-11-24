04316345f178b68f312ece58a9c07af609fba26a
Rasedur Rahman Roxy <roxy.xprez@gmail.com>
2023-11-24 08:27:29 +0600

    settings

--- apps/kathanika-ils/src/app/app.component.ts
+++ apps/kathanika-ils/src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
-import { KnUiComponent } from '@kathanika/kn-ui';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule, KnUiComponent],
+  imports: [NxWelcomeComponent, RouterModule],
   selector: 'kathanika-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
