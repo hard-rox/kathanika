5234952dca04a154621c707c8d918d95fef61384
Rasedur Rahman Roxy <roxy.xprez@gmail.com>
2023-11-25 08:27:42 +0600

    graphql module

--- apps/kathanika-ils/src/app/app.config.ts
+++ apps/kathanika-ils/src/app/app.config.ts
@@ -1,12 +1,20 @@
-import { ApplicationConfig, isDevMode } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideServiceWorker } from '@angular/service-worker';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { GraphQLModule } from '@kathanika/graphql-consumer';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideClientHydration(), provideRouter(appRoutes), provideServiceWorker('ngsw-worker.js', {
+  providers: [
+    provideClientHydration(),
+    provideRouter(appRoutes),
+    provideServiceWorker('ngsw-worker.js', {
         enabled: !isDevMode(),
         registrationStrategy: 'registerWhenStable:30000'
-    })],
+    }),
+    provideHttpClient(withFetch()),
+    importProvidersFrom(GraphQLModule)
+  ],
 };
--- libs/client/shared/graphql-consumer/src/index.ts
+++ libs/client/shared/graphql-consumer/src/index.ts
@@ -1,1 +1,2 @@
 export * from './lib/graphql/generated/graphql-operations';
+export * from './lib/graphql.module';
