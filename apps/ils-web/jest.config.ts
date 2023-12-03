5a3e8127f4c18ff6bbc4ea9c0ad183f6b9643d55
Rasedur Rahman Roxy <roxy.xprez@gmail.com>
2023-12-03 07:12:42 +0600

    build(ils): :art: rename ils to ils-web

--- apps/ils-web/jest.config.ts
+++ apps/ils-web/jest.config.ts
@@ -1,9 +1,9 @@
 /* eslint-disable */
 export default {
-  displayName: 'ils',
+  displayName: 'ils-web',
   preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
-  coverageDirectory: '../../coverage/apps/ils',
+  coverageDirectory: '../../coverage/apps/ils-web',
   transform: {
     '^.+\\.(ts|mjs|js|html)$': [
       'jest-preset-angular',
