844de1ac75705e2a62af5a31a9418924b6f5fc5d
Rasedur Rahman Roxy <roxy.xprez@gmail.com>
2023-12-03 07:01:10 +0600

    build(ils): :art: rename ils from text content

--- apps/ils-e2e/src/e2e/app.cy.ts
+++ apps/ils-e2e/src/e2e/app.cy.ts
@@ -1,6 +1,6 @@
 import { getGreeting } from '../support/app.po';
 
-describe('kathanika-ils-e2e', () => {
+describe('ils-e2e', () => {
   beforeEach(() => cy.visit('/'));
 
   it('should display welcome message', () => {
--- apps/ils/jest.config.ts
+++ apps/ils/jest.config.ts
@@ -1,6 +1,6 @@
 /* eslint-disable */
 export default {
-  displayName: 'kathanika-ils',
+  displayName: 'ils',
   preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   coverageDirectory: '../../coverage/apps/ils',
