94782da183875dc3ea211cefc267b9b1fc20b720
Rasedur Rahman Roxy <roxy.xprez@gmail.com>
2023-12-21 08:09:30 +0600

    fix(web-service): :bug: audit properties on insert and update
    Add audit properties in AggregateRoot

--- libs/server/domain/Primitives/AggregateRoot.cs
+++ libs/server/domain/Primitives/AggregateRoot.cs
@@ -2,22 +2,29 @@ namespace Kathanika.Domain.Primitives;
 
 public abstract class AggregateRoot : Entity
 {
-    private List<IDomainEvent> domainEvents = new();
+    private List<IDomainEvent> domainEvents = [];
+
+    public string CreatedByUserId { get; private init; } = string.Empty;
+    public string CreatedByUserName { get; private init; } = string.Empty;
+    public DateTimeOffset CreatedAt { get; private init; } = DateTimeOffset.MinValue;
+    public string? LastModifiedByUserId { get; private set; }
+    public string? LastModifiedByUserName { get; private set; }
+    public DateTimeOffset? LastModifiedAt { get; private set; }
 
     protected void AddDomainEvent(IDomainEvent domainEvent)
     {
-        domainEvents ??= new();
+        domainEvents ??= [];
         domainEvents.Add(domainEvent);
     }
 
     public List<IDomainEvent> GetDomainEvents()
     {
-        return domainEvents ?? new();
+        return domainEvents ?? [];
     }
 
     public void ClearDomainEvents()
     {
-        domainEvents ??= new();
+        domainEvents ??= [];
         domainEvents.Clear();
     }
 }
