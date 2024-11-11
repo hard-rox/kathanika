f4d9a207358a4c86ca5aa0a3b2e9298e64cee8b8
Rasedur Rahman Roxy <roxy.xprez@gmail.com>
2024-11-11 07:42:30 +0600

    feat: app components

--- src/app-projects/ils-web/src/app/app.component.ts
+++ src/app-projects/ils-web/src/app/app.component.ts
@@ -1,18 +1,21 @@
 import {Component} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
-import {KnFileInput} from "@kathanika/kn-ui";
+import {HeaderComponent} from "./header/header.component";
+import {SidebarComponent} from "./sidebar/sidebar.component";
+import {FooterComponent} from "./footer/footer.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [RouterOutlet],
-    template: `<!--<kathanika-header></kathanika-header>-->
-    <!--    <kathanika-sidebar></kathanika-sidebar>-->
-    <main class="pl-72 py-20 pr-4">
-        <router-outlet></router-outlet>
-    </main>
-<!--    <kn-file-input></kn-file-input>-->
-    <!--<kathanika-footer></kathanika-footer>-->`
+    imports: [RouterOutlet, HeaderComponent, SidebarComponent, FooterComponent],
+    template: `
+        <app-header></app-header>
+        <app-sidebar></app-sidebar>
+        <main class="pl-72 py-20 pr-4">
+            <router-outlet></router-outlet>
+        </main>
+        <app-footer></app-footer>
+    `
 })
 export class AppComponent {
     title = 'ils-web';
---
+++ src/app-projects/ils-web/src/app/header/header.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import {KnButton} from "@kathanika/kn-ui";
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [
+      KnButton
+  ],
+  templateUrl: './header.component.html'
+})
+export class HeaderComponent {
+    isUserActionsVisible = false;
+    isNotificationVisible = false;
+}
