8a940fdf392f659d43ddbf9287f4116f983999d8
Rasedur Rahman Roxy <roxy.xprez@gmail.com>
2024-11-12 07:26:09 +0600

    jest config change

--- jest.config.ts
+++ jest.config.ts
@@ -95,7 +95,9 @@ const config: Config = {
   },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
-  // modulePathIgnorePatterns: [],
+  modulePathIgnorePatterns: [
+      "<rootDir>/dist/"
+  ],
 
   // Activates notifications for test results
   // notify: false,
