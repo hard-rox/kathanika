d5a416682636eb0341dcfd066d5fb25f2d83dd4f
Rasedur Rahman Roxy <roxy.xprez@gmail.com>
2024-11-16 09:26:55 +0600

    feat: graphql generalization of entity and aggregate root types

--- src/core/Kathanika.Domain/Primitives/AggregateRoot.cs
+++ src/core/Kathanika.Domain/Primitives/AggregateRoot.cs
@@ -1,15 +1,26 @@
+using HotChocolate;
+
 namespace Kathanika.Domain.Primitives;
 
 public abstract class AggregateRoot : Entity
 {
     private List<IDomainEvent> _domainEvents = [];
 
+    [GraphQLIgnore]
     public string CreatedByUserId { get; private init; } = string.Empty;
+    [GraphQLIgnore]
     public string CreatedByUserName { get; private init; } = string.Empty;
+    [GraphQLIgnore]
     public DateTimeOffset CreatedAt { get; private init; } = DateTimeOffset.MinValue;
+    [GraphQLIgnore]
     public string? LastModifiedByUserId { get; private set; }
+    [GraphQLIgnore]
     public string? LastModifiedByUserName { get; private set; }
+    [GraphQLIgnore]
     public DateTimeOffset? LastModifiedAt { get; private set; }
+    
+    [GraphQLIgnore]
+    public IReadOnlyList<IDomainEvent> DomainEvents => _domainEvents;
 
     protected void AddDomainEvent(IDomainEvent domainEvent)
     {
@@ -17,11 +28,7 @@ public abstract class AggregateRoot : Entity
         _domainEvents.Add(domainEvent);
     }
 
-    public List<IDomainEvent> GetDomainEvents()
-    {
-        return _domainEvents ?? [];
-    }
-
+    [GraphQLIgnore]
     public void ClearDomainEvents()
     {
         _domainEvents ??= [];
--- src/core/Kathanika.Domain/Primitives/ValueObject.cs
+++ src/core/Kathanika.Domain/Primitives/ValueObject.cs
@@ -1,20 +1,26 @@
+using HotChocolate;
+
 namespace Kathanika.Domain.Primitives;
 
 public abstract record ValueObject : IEquatable<ValueObject>
 {
+    [GraphQLIgnore]
     public abstract IEnumerable<object> GetAtomicValues();
 
+    [GraphQLIgnore]
     private bool ValuesAreEqual(ValueObject other)
     {
         return GetAtomicValues().SequenceEqual(other.GetAtomicValues());
     }
 
+    [GraphQLIgnore]
     public virtual bool Equals(ValueObject? other)
     {
         return other is not null
             && ValuesAreEqual(other);
     }
 
+    [GraphQLIgnore]
     public override int GetHashCode()
     {
         return GetAtomicValues()
