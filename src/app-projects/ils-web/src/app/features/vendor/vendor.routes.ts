33659f444231a86b420c44dbf3f673edb2c5ed8f
Rasedur Rahman Roxy <roxy.xprez@gmail.com>
2024-12-07 09:10:42 +0600

    lint: lint fix

--- src/app-projects/ils-web/src/app/features/vendor/vendor.routes.ts
+++ src/app-projects/ils-web/src/app/features/vendor/vendor.routes.ts
@@ -1,8 +1,4 @@
 import {Routes} from "@angular/router";
-import {VendorListComponent} from "./vendor-list/vendor-list.component";
-import {VendorDetailsComponent} from "./vendor-details/vendor-details.component";
-import {VendorAddComponent} from "./vendor-add/vendor-add.component";
-import {VendorUpdateComponent} from "./vendor-update/vendor-update.component";
 
 export const vendorRoutes: Routes = [
     {
