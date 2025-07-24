a0d77a62845dbbf0408caee68c65eed62e828519
Rasedur Rahman Roxy <roxy.xprez@gmail.com>
2025-07-24 08:02:57 +0600

    refactor: change ItemVendor to a sealed record with private setters and internal constructor

--- src/core/Kathanika.Domain/Aggregates/BibItemAggregate/ItemVendor.cs
+++ src/core/Kathanika.Domain/Aggregates/BibItemAggregate/ItemVendor.cs
@@ -2,8 +2,20 @@ using Kathanika.Domain.Primitives;
 
 namespace Kathanika.Domain.Aggregates.BibItemAggregate;
 
-public record ItemVendor(string Id, string Name) : ValueObject
+public sealed record ItemVendor : ValueObject
 {
+    public string Id { get; private set; }
+    public string Name { get; private set; }
+    private ItemVendor()
+    {
+    }
+    
+    internal ItemVendor(string id, string name)
+    {
+        Id = id;
+        Name = name;
+    }
+
     public override IEnumerable<object?> GetAtomicValues()
     {
         yield return Id;
