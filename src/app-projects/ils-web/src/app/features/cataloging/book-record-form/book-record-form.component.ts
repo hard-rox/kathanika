ff4f87fa6b6d7eeb5202a2a957461c0dd99ddc1d
Rasedur Rahman Roxy <roxy.xprez@gmail.com>
2025-07-31 08:26:18 +0600

    feat: redesign `BibRecordCreateComponent` and enhance `BookRecordFormComponent` for better usability
    - Updated `BibRecordCreateComponent` layout with a more intuitive panel structure and improved callouts for adding books.
    - Enhanced `BookRecordFormComponent` with additional fields (`language`, `edition`, `category`, `description`) and `helpText` property for guidance.
    - Refined form structure into logical groupings and added contextual help for a better user experience.

--- src/app-projects/ils-web/src/app/features/cataloging/book-record-form/book-record-form.component.ts
+++ src/app-projects/ils-web/src/app/features/cataloging/book-record-form/book-record-form.component.ts
@@ -2,7 +2,7 @@ import {Component} from '@angular/core';
 import {BaseFormComponent, FormControlsOf} from "../../../abstractions/base-form-component";
 import {CreateBibRecordInput} from "../../../graphql/generated/graphql-operations";
 import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
-import {KnButton, KnTextInput} from "@kathanika/kn-ui";
+import {KnButton, KnTextareaInput, KnTextInput} from "@kathanika/kn-ui";
 import {CommonModule} from "@angular/common";
 
 @Component({
@@ -11,21 +11,25 @@ import {CommonModule} from "@angular/common";
         CommonModule,
         ReactiveFormsModule,
         KnTextInput,
-        KnButton
+        KnButton,
+        KnTextareaInput
     ],
     standalone: true,
     templateUrl: './book-record-form.component.html'
 })
 export class BookRecordFormComponent extends BaseFormComponent<CreateBibRecordInput> {
-    protected override createFormGroup(): FormGroup<FormControlsOf<CreateBibRecordInput>> {
-        return new FormGroup<FormControlsOf<CreateBibRecordInput>>({
+    protected override createFormGroup(): FormGroup {
+        return new FormGroup({
             title: new FormControl<string>('', {nonNullable: true, validators: [Validators.required]}),
             isbn: new FormControl<string | null>(null),
             author: new FormControl<string | null>(null),
             publisherName: new FormControl<string | null>(null),
             publicationDate: new FormControl<string | null>(null),
             extent: new FormControl<string | null>(null),
-            dimensions: new FormControl<string | null>(null),
+            language: new FormControl<string | null>(null),
+            edition: new FormControl<string | null>(null),
+            category: new FormControl<string | null>(null),
+            description: new FormControl<string | null>(null),
             coverImageId: new FormControl<string | null>(null)
         });
     }
